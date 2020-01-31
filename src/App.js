8aac753bfe64cf8dcc638ee5f258f5227b22d23d
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-31 10:25:32 -0600

    completed the app jsx. and setup yarn.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
 
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import LocationList from "./components/LocationsList";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/characters" component={CharacterList} />
+      <Route exact path="/locations" component={LocationList} />
     </main>
   );
 }
